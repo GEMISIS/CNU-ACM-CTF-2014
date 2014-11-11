b7e7b31297bf84d535adebacb07ec57497c77fbb
Gerald McAlister <gerald.mcalister.gem@gmail.com>
2014-11-11 10:33:31 -0500

    Initial Creation
    -Organized via folders to keep everything clean
    -Added basic buffer overflow exploits, and a simple integer underflow
    exploit

---
+++ Buffer Overflows/Happy Buffday/Easy/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void getUser(char* username)
+{
+	strcpy(username, "noone");
+}
+
+int main(int argc, char* argv[])
+{
+	char username[16];
+	char message[32];
+
+	getUser(username);
+
+	printf("Enter a 32 letter message for your birthday:");
+	gets(message);
+
+	if(!strcmp(username, "root"))
+	{
+		printf("You got a flag!");
+	}
+	else
+	{
+		printf("Message set to %s!", message);
+	}
+	return 0;
+}
